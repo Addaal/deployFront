7f06e21e866069466d2b637f71dd132a2c70fab5
Addaal <adalmontania@gmail.com>
2023-11-26 10:47:06 -0300

    rsspddt

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -9,11 +9,14 @@ export const AuthContextProvider = ({children}) =>{
 
     const login = async(inputs) =>{
         const res = await axios.post("https://app-blog-hopeitowkrs-fde410f2c677.herokuapp.com/api/auth/login", inputs)
+        console.log("login")
+        console.log(res)
         setCurrentUser(res.data)
     }
 
     const logout = async(inputs) =>{
         const res = await axios.post("https://app-blog-hopeitowkrs-fde410f2c677.herokuapp.com/api/auth/logout")
+      
         setCurrentUser("");
     }
 
